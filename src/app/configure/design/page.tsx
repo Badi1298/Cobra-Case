81b00cc6cef9f63aa73c19a3c92e790bffcc48f6
Serban23 <serban.david@flowmatters.com>
2024-05-29 12:05:38 +0300

    4:40:35

--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,3 +1,9 @@
+import { notFound } from 'next/navigation';
+
+import db from '@/lib/db';
+
+import DesignConfigurator from './design-configurator';
+
 type PageProps = {
 	searchParams: {
 		[key: string]: string | string[] | undefined;
@@ -7,5 +13,21 @@ type PageProps = {
 export default async function Page({ searchParams }: PageProps) {
 	const { id } = searchParams;
 
-	return <div>Page</div>;
+	if (!id || typeof id !== 'string') return notFound();
+
+	const configuration = await db.configuration.findUnique({
+		where: { id },
+	});
+
+	if (!configuration) return notFound();
+
+	const { imageUrl, width, height } = configuration;
+
+	return (
+		<DesignConfigurator
+			configId={id}
+			imageUrl={imageUrl}
+			imageDimensions={{ width, height }}
+		/>
+	);
 }
